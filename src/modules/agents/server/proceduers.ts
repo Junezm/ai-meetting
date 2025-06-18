43ba04ac5527d78606e2467f0d9112534aa8fe1e
JuneChang <junezm@foxmail.com>
2025-06-18 22:28:20 +0800

    feat(agents): 添加代理创建功能及权限验证
    - 新增agents模块的schema验证
    - 在trpc中实现protectedProcedure进行权限验证
    - 添加代理创建接口，关联用户ID

--- src/modules/agents/server/proceduers.ts
+++ src/modules/agents/server/proceduers.ts
@@ -1,9 +1,11 @@
 import { db } from '@/db';
 import { agents } from '@/db/schema';
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from '../schema';
 // import { TRPCError } from '@trpc/server';
 
 export const agentsRouter = createTRPCRouter({
+  // todo change getmany to use protectedProcedure
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
     // await new Promise((resolve) => {
@@ -13,5 +15,20 @@ export const agentsRouter = createTRPCRouter({
     // })
     // throw new TRPCError({ code: 'BAD_REQUEST' });
     return data;
-  })
+  }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx}) => {
+      // const { name, instructions } = input;
+      // const { auth } = ctx;
+      const [createAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      return createAgent;  
+    })
 })
